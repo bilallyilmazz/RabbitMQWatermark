8cf7e433e7ab9bce6f0233b7194995ffaf1ff629
Bilal Yılmaz <bilallyilmazz@outlook.com>
2023-01-05 01:43:15 +0300

    Add project files.

---
+++ Web/Models/Services/RabbitMQPublisher.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+namespace Web.Models.Services
+{
+    public class RabbitMQPublisher
+    {
+        private readonly RabbitMQClienService _rabbitMQClienService;
+
+        public RabbitMQPublisher(RabbitMQClienService rabbitMQClienService)
+        {
+            _rabbitMQClienService = rabbitMQClienService;
+        }
+
+        public void Publish(ProductImageCreatedEvent productImageCreatedEvent)
+        {
+            var channel = _rabbitMQClienService.Connect();
+            var bodyString = JsonSerializer.Serialize(productImageCreatedEvent);
+            var bodyByte=Encoding.UTF8.GetBytes(bodyString);
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+
+            channel.BasicPublish(exchange:RabbitMQClienService.ExchangeName,routingKey:RabbitMQClienService.RoutingWatermark, basicProperties: properties, body: bodyByte);
+        }
+    }
+}
---
+++ Web/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using RabbitMQ.Client;
+using Web.Models;
+using Web.Models.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton(sp => new ConnectionFactory() { Uri = new Uri(builder.Configuration.GetConnectionString("RabbitMq")) });
+
+builder.Services.AddSingleton<RabbitMQClienService>();
+builder.Services.AddSingleton<RabbitMQPublisher>();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseInMemoryDatabase(databaseName: "productDb");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
