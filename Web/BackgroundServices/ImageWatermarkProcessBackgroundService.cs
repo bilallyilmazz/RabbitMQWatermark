f82ac9ce93ded6982d658b649bda97d600c10210
Bilal Yılmaz <bilallyilmazz@outlook.com>
2023-01-08 01:11:07 +0300

    Watermark completed

---
+++ Web/BackgroundServices/ImageWatermarkProcessBackgroundService.cs
@@ -0,0 +1,82 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Configuration;
+using System.Drawing;
+using System.Text;
+using System.Text.Json;
+using Web.Models.Services;
+
+namespace Web.BackgroundServices
+{
+    public class ImageWatermarkProcessBackgroundService : BackgroundService
+    {
+        private readonly  RabbitMQClienService _clientService;
+        private readonly ILogger<ImageWatermarkProcessBackgroundService> _logger;
+        private IModel _channel;
+        public ImageWatermarkProcessBackgroundService(RabbitMQClienService clientService, ILogger<ImageWatermarkProcessBackgroundService> logger)
+        {
+            _clientService = clientService;
+            _logger = logger;
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _channel=_clientService.Connect();
+            _channel.BasicQos(0, 1, false);
+
+
+            return base.StartAsync(cancellationToken);
+        }
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var consumer = new AsyncEventingBasicConsumer(_channel);
+            _channel.BasicConsume(RabbitMQClienService.QueueName,false,consumer);
+            consumer.Received += Consumer_Received;
+            return Task.CompletedTask;
+        }
+
+        private Task Consumer_Received(object sender, BasicDeliverEventArgs @event)
+        {
+            try
+            {
+                var imageCreatedEvent = JsonSerializer.Deserialize<ProductImageCreatedEvent>(Encoding.UTF8.GetString(@event.Body.ToArray()));
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", imageCreatedEvent.ImageName);
+
+                var watermarkValue = "www.mysitem.com";
+                using var img = Image.FromFile(path);
+                using var graphic = Graphics.FromImage(img);
+
+                var font = new Font(FontFamily.GenericMonospace, 32, FontStyle.Bold, GraphicsUnit.Pixel);
+
+                var textSize = graphic.MeasureString(watermarkValue, font);
+                var color = Color.FromArgb(247, 9, 45);
+                var brush = new SolidBrush(color);
+
+                var position = new Point(img.Width - ((int)textSize.Width + 30), img.Height - ((int)textSize.Height + 30));
+
+                graphic.DrawString(watermarkValue, font, brush, position);
+
+                img.Save("wwwroot/images/watermarks/" + imageCreatedEvent.ImageName);
+
+                img.Dispose();
+                graphic.Dispose();
+
+                _channel.BasicAck(@event.DeliveryTag, false);
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError(ex.Message);
+            }
+
+            return Task.CompletedTask;
+
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            return base.StopAsync(cancellationToken);
+        }
+    }
+}
--- Web/Program.cs
+++ Web/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using RabbitMQ.Client;
+using Web.BackgroundServices;
 using Web.Models;
 using Web.Models.Services;
 
@@ -7,11 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddSingleton(sp => new ConnectionFactory() { Uri = new Uri(builder.Configuration.GetConnectionString("RabbitMq")) });
+builder.Services.AddSingleton(sp => new ConnectionFactory() { Uri = new Uri(builder.Configuration.GetConnectionString("RabbitMq")),DispatchConsumersAsync=true });
 
 builder.Services.AddSingleton<RabbitMQClienService>();
 builder.Services.AddSingleton<RabbitMQPublisher>();
 
+builder.Services.AddHostedService<ImageWatermarkProcessBackgroundService>();
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseInMemoryDatabase(databaseName: "productDb");
